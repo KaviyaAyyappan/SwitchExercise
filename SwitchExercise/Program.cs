049ce31a692f55ffa13b0ac1207b16faa7af4f8a
Kaviya Ayyappan <kaviya.eie@gmail.com>
2020-05-13 13:09:17 +0200

    Switch exercise using switch

---
+++ SwitchExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchExercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+                Console.WriteLine("Enter the result:");
+                int result = int.Parse(Console.ReadLine());
+                switch (result)
+                {
+                    case 10:
+                    case 9:
+                        Console.WriteLine("Excellent");
+                        break;
+
+                    case 8:
+                    case 7:
+                        Console.WriteLine("Notable");
+                        break;
+                    case 6:
+                        Console.WriteLine("Good");
+                        break;
+                    case 5:
+                        Console.WriteLine("pass");
+                        break;
+                    default:
+                        Console.WriteLine("Fail");
+                        break;
+                }
+                
+                Console.Read();
+            }
+    }
+}
